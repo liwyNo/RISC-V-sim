158551f7757da088954c6d0ea6b32e7a8e6a148d
Wangyang Li <lee.wangyang@gmail.com>
2016-11-20 02:27:24 +0800

    Try to run faster.

--- vmm.h
+++ vmm.h
@@ -14,24 +14,29 @@
 struct block;
 struct VM {
     unsigned long long TLB[3];
+    unsigned char* TLBB[3];
     std::unordered_map<unsigned long long, unsigned char*> vm_pool;
     block operator[](unsigned long long offset);
     VM(){
-        vm_pool[0] = new unsigned char[PAGE_SIZE];
+        unsigned char* tmp = vm_pool[0] = new unsigned char[PAGE_SIZE];
+        for(int i = 0; i < 3; ++i){
+            TLB[i] = 0;
+            TLBB[i] = tmp;
+        }
     }
     bool load(const char* src, unsigned long long des, unsigned long long len) {
         while (1) {
             if (((des + len) & TAG_MASK) != (des & TAG_MASK)) {
                 auto a_len = PAGE_SIZE - (des & PAGE_MASK);
-                exist(des & TAG_MASK);
-                memcpy(vm_pool[des & TAG_MASK] + (des & PAGE_MASK), src, a_len);
+                auto base = exist(des & TAG_MASK);
+                memcpy(base + (des & PAGE_MASK), src, a_len);
                 src += a_len;
                 des += a_len;
                 len -= a_len;
                 continue;
             } else {
-                exist(des & TAG_MASK);
-                memcpy(vm_pool[des & TAG_MASK] + (des & PAGE_MASK), src, len);
+                auto base = exist(des & TAG_MASK);
+                memcpy(base + (des & PAGE_MASK), src, len);
                 break;
             }
         }
@@ -41,15 +46,15 @@ struct VM {
         while (1) {
             if (((des + len) & TAG_MASK) != (des & TAG_MASK)) {
                 auto a_len = PAGE_SIZE - (des & PAGE_MASK);
-                exist(des & TAG_MASK);
-                memcpy(src, vm_pool[des & TAG_MASK] + (des & PAGE_MASK), a_len);
+                auto base = exist(des & TAG_MASK);
+                memcpy(src, base + (des & PAGE_MASK), a_len);
                 src += a_len;
                 des += a_len;
                 len -= a_len;
                 continue;
             } else {
-                exist(des & TAG_MASK);
-                memcpy(src, vm_pool[des & TAG_MASK] + (des & PAGE_MASK), len);
+                auto base = exist(des & TAG_MASK);
+                memcpy(src, base + (des & PAGE_MASK), len);
                 break;
             }
         }
@@ -59,28 +64,33 @@ struct VM {
         while (1) {
             if ((des + len) >= ((des & TAG_MASK) + PAGE_SIZE)) {
                 auto a_len = PAGE_SIZE - (des & PAGE_MASK);
-                exist(des & TAG_MASK);
-                memset(vm_pool[des & TAG_MASK] + (des & PAGE_MASK), 0, a_len);
+                auto base = exist(des & TAG_MASK);
+                memset(base + (des & PAGE_MASK), 0, a_len);
                 des += a_len;
                 len -= a_len;
                 continue;
             } else {
-                exist(des & TAG_MASK);
-                memset(vm_pool[des & TAG_MASK] + (des & PAGE_MASK), 0, len);
+                auto base = exist(des & TAG_MASK);
+                memset(base + (des & PAGE_MASK), 0, len);
                 break;
             }
         }
         return true;
     }
-    void exist(unsigned long long tag) {
+
+    unsigned char * exist(unsigned long long tag) {
         for(int i = 0; i < 3; ++i)
             if(TLB[i] == tag)
-                return;
+                return TLBB[i];
         if (vm_pool.find(tag) == vm_pool.end())
             vm_pool[tag] = new unsigned char[PAGE_SIZE];
-        for(int i = 0; i < 2; ++i)
+        for(int i = 0; i < 2; ++i) {
             TLB[i + 1] = TLB[i];
+            TLBB[i + 1] = TLBB[i];
+        }
         TLB[0] = tag;
+        TLBB[0] = vm_pool[tag];
+        return TLBB[0];
     }
 };
 struct block {
@@ -92,13 +102,14 @@ struct block {
             unsigned char byte[8];
             unsigned long long dword;
         } tmp;
-        vm->exist(offset & TAG_MASK);
-        vm->exist((offset + sizeof(unsigned long long)) & TAG_MASK);
+        //vm->exist(offset & TAG_MASK);
+        //vm->exist((offset + sizeof(unsigned long long)) & TAG_MASK);
         for (int i = 0; i < 8; ++i) {
             auto now_off = offset + i;
             auto tag = now_off & TAG_MASK;
             now_off &= PAGE_MASK;
-            tmp.byte[i] = vm->vm_pool[tag][now_off];
+            auto base = vm->exist(tag);
+            tmp.byte[i] = base[now_off];
         }
         return tmp.dword;
     }
@@ -108,13 +119,14 @@ struct block {
             unsigned long long dword;
         } tmp;
         tmp.dword = content;
-        vm->exist(offset & TAG_MASK);
-        vm->exist((offset + sizeof(unsigned long long)) & TAG_MASK);
+        //vm->exist(offset & TAG_MASK);
+        //vm->exist((offset + sizeof(unsigned long long)) & TAG_MASK);
         for (int i = 0; i < 8; ++i) {
             auto now_off = offset + i;
             auto tag = now_off & TAG_MASK;
             now_off &= PAGE_MASK;
-            vm->vm_pool[tag][now_off] = tmp.byte[i];
+            auto base = vm->exist(tag);
+            base[now_off] = tmp.byte[i];
         }
         return *this;
     }
